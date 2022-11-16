65112809f44142cd28e4d60c91a34510f48dde1b
lichaoran <pkwarcraft@gmail.com>
2022-11-16 17:13:11 +0800

    no arm for now
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- generate_rootfs.sh
+++ generate_rootfs.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-if [[ "$(whoami)" != "root" ]] && [[ "$(whoami)" != "Administrator" ]]; then
-    echo please run in privileged user!
-    echo you are `whoami`
-    exit 1
-fi
+
 ARGS=""
 if [ ! -z $1 ];then
     ARGS="--build-args REL_TAG=$1"
