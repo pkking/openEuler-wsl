65c36079aa4d7ec44e2b4f96d2aca0375ca48e59
lichaoran <pkwarcraft@gmail.com>
2023-01-03 17:43:55 +0800

    prepare jenkins jobs
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- jenkins/rootfs/prepare_job.sh
+++ jenkins/rootfs/prepare_job.sh
@@ -3,12 +3,12 @@
 # prepare buildx 
 docker buildx install
 docker run --privileged --rm tonistiigi/binfmt --install all
-[ -d $WORKSPACE_TMP/outdir/ ] && rm -rvf $WORKSPACE_TMP/outdir/* || mkdir -pv $WORKSPACE_TMP/outdir
+[ -d $WORKSPACE/outdir/ ] && rm -rvf $WORKSPACE/outdir/* || mkdir -pv $WORKSPACE/outdir
 
 [ -z "$(docker buildx ls |grep multiarch)" ] && docker buildx create --use --name multiarch
 docker buildx ls
 
 # build rootfs docker image
 docker buildx build --build-arg REL_TAG=$release --platform linux/aarch64,linux/amd64 --tag openeuler-wsl:$release --squash --cache-from=type=local,src=/var/cache/buildx/$release --cache-to=type=local,dest=/var/cache/buildx/$release \
-    -o type=tar,dest=$WORKSPACE_TMP/outdir/$release.tar $WORKSPACE/docker/
-docker run --rm -v $WORKSPACE_TMP/:/wd bytesco/pigz -9 -v -Y -f /wd/outdir/$release.tar
+    -o type=tar,dest=$WORKSPACE/outdir/$release.tar $WORKSPACE/docker/
+docker run --rm -v $WORKSPACE/:/wd bytesco/pigz -9 -v -Y -f /wd/outdir/$release.tar
