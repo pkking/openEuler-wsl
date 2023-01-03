3a648c6b094488c554d418ab8b4521ee732a3775
lichaoran <pkwarcraft@gmail.com>
2023-01-03 14:38:40 +0800

    prepare jenkins jobs
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- jenkins/rootfs/prepare_job.sh
+++ jenkins/rootfs/prepare_job.sh
@@ -11,4 +11,4 @@ docker buildx ls
 # build rootfs docker image
 docker buildx build --build-arg REL_TAG=$release --platform linux/aarch64,linux/amd64 --tag openeuler-wsl:$release --squash --cache-from=type=local,src=/var/cache/buildx/$release --cache-to=type=local,dest=/var/cache/buildx/$release \
     -o type=tar,dest=$WORKSPACE_TMP/outdir/$release.tar $WORKSPACE/docker/
-docker run --rm -v $WORKSPACE_TMP/:/wd ghcr.io/kasperskytte/docker-pigz:master -11 /wd/outdir/$release.tar
+docker run --rm -v $WORKSPACE_TMP/:/wd bytesco/pigz -11 /wd/outdir/$release.tar
