0750dffce30f766c24236966e4bbf7fb272acc26
lichaoran <pkwarcraft@gmail.com>
2023-01-09 10:25:24 +0800

    add app pipline
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- jenkins/rootfs/do_build_rootfs.sh
+++ jenkins/rootfs/do_build_rootfs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 [ -d $WORKSPACE/outdir/ ] && rm -rvf $WORKSPACE/outdir/* || mkdir -pv $WORKSPACE/outdir
-
-[ -z "$(docker buildx ls |grep multiarch)" ] && docker buildx create --use --name multiarch || docker buildx use --name multiarch
+docker buildx ls
+docker ps
+[ -z "$(docker buildx ls |grep multiarch)" ] && docker buildx create --use --name multiarch
 
 # build rootfs docker image
 docker buildx build --build-arg REL_TAG=$release --platform linux/$arch --tag openeuler-wsl:$release --squash --cache-from=type=local,src=/var/cache/buildx/$release-$arch --cache-to=type=local,dest=/var/cache/buildx/$release-$arch \
