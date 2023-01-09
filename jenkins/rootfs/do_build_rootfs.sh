5bb8923c032789fc382c041a41d77ed39cef2abf
lichaoran <pkwarcraft@gmail.com>
2023-01-09 10:10:41 +0800

    add app pipline
    Signed-off-by: lichaoran <pkwarcraft@gmail.com>

--- jenkins/rootfs/do_build_rootfs.sh
+++ jenkins/rootfs/do_build_rootfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -d $WORKSPACE/outdir/ ] && rm -rvf $WORKSPACE/outdir/* || mkdir -pv $WORKSPACE/outdir
+
 # build rootfs docker image
 docker buildx build --build-arg REL_TAG=$release --platform linux/$arch --tag openeuler-wsl:$release --squash --cache-from=type=local,src=/var/cache/buildx/$release-$arch --cache-to=type=local,dest=/var/cache/buildx/$release-$arch \
     -o type=tar,dest=$WORKSPACE/outdir/$release-$arch.tar $WORKSPACE/docker/
--- jenkins/rootfs/prepare_job.sh
+++ jenkins/rootfs/prepare_job.sh
@@ -3,7 +3,6 @@
 # prepare buildx 
 docker buildx install
 docker run --privileged --rm tonistiigi/binfmt --install all
-[ -d $WORKSPACE/outdir/ ] && rm -rvf $WORKSPACE/outdir/* || mkdir -pv $WORKSPACE/outdir
 
 # clean old buildx tool
 docker buildx rm multiarch
